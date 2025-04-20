21890d09b6ecfa83bd8e19e87d581704ad299588
Lucas Ferreira <lucasgomm33@gmail.com>
2025-04-20 18:06:56 -0300

    add Hero section in landing page and create ui components default

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import Hero from "./components/landing-page/Hero";
+
 export default function Home() {
   return (
-   <div>Hello</div>
+   <div className="max-w-7xl mx-auto border">
+    <Hero />
+    {/* <Header/>
+    
+    <VideoExplanation/>
+    <Pricing/>
+    <FAQ/> */}
+   </div>
   );
 }
